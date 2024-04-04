834d60bc01fef4f4e31fe7655b618edf5b04d605
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-04-04 22:39:01 +0900

    kadai_020提出用

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const textElement = document.getElementById('text');
+const btnElement = document.getElementById('btn');
+
+btnElement.addEventListener('click',()=> {
+  textElement.textContent = 'ボタンをクリックしました';
+  console.log(textElement);
+  });
